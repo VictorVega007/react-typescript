23e0613530dc541c405ab3857d53657bb883d779
victorv <victorvega.v@gmail.com>
2023-02-21 16:50:59 -0300

    Adding interfaces

--- src/02-component-pattern/components/index.ts
+++ src/02-component-pattern/components/index.ts
@@ -2,13 +2,14 @@ import { ProductCard as ProductCardHOC } from "./ProductCard";
 import { ProductTitle } from "./ProductTitle";
 import { ProductButtons } from "./ProductsButtons";
 import { ProductImage } from "./ProductImage";
+import { ProductCardHOCProps } from "../interfaces/interfaces";
 
 export { ProductButtons } from "./ProductsButtons";
 // export { ProductCard } from "./ProductCard";
 export { ProductImage } from "./ProductImage";
 export { ProductTitle } from "./ProductTitle";
 
-export const ProductCard = Object.assign(ProductCardHOC, {
+export const ProductCard: ProductCardHOCProps = Object.assign(ProductCardHOC, {
   Title: ProductTitle,
   Buttons: ProductButtons,
   Image: ProductImage,
